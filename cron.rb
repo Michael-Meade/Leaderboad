ba0a2b6f263670d68551210548f3e77eed4338f0
Michael-Meade <mameade@utica.edu>
2020-03-06 23:54:46 -0500

    will alert admin if the scroing file has a 403 error

--- cron.rb
+++ cron.rb
@@ -1,10 +1,22 @@
 require_relative 'sql/db'
 require_relative 'lib/lb'
+require 'pp'
 require 'httparty'
 begin
-	response  = HTTParty.get("IP")
+	response  = HTTParty.get("http://104.31.72.21/")
 	team_name = response.parsed_response.to_s
-	DB.add_points(team_name)
+	if response.code.to_s == "403"
+		# if cant access to scoring file
+		# will send message in discord channel that
+		# will alert admin of errors, if its enabled in the configs
+		Alerts.check_status("403 error with scoring file.", "cron.rb")
+	else
+		# everything goes right and the users get their pooints.
+		DB.add_points(team_name)
+	end
 rescue => e
+	# alert the asdmin that there was a error with scoring.
+	# sends alert in discord channels
 	Alerts.check_status(e, "cron.rb")
 end
+
